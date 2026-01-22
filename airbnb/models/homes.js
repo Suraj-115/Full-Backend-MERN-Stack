aa4d4467ec719d1c557f8e7f0ff108a31d1b51bd
Suraj-115 <agarwalsuraj863@gmail.com>
2026-01-22 17:03:08 +0530

    Home Added Addition to File

--- airbnb/models/homes.js
+++ airbnb/models/homes.js
@@ -1,5 +1,8 @@
-
-
+// Core modules
+const fs = require("fs");
+const path = require("path");
+const rootDir = require('../utils/pathUtil');
+// fake database
 const registeredHomes = [];
 
 module.exports = class Home {
@@ -11,6 +14,10 @@ module.exports = class Home {
   }
   save(){
     registeredHomes.push(this);
+    const homeDataPath = path.join(rootDir,'data','homes.json');
+    fs.writeFile(homeDataPath,JSON.stringify(registeredHomes),err =>{
+      console.log("File Writing Concluded", err);
+    });
   }
   static fetchAll(){
     return registeredHomes;
