14fd30fccad6fceb1c2dbb296c63c869333609c0
Suraj-115 <agarwalsuraj863@gmail.com>
2026-03-08 15:52:46 +0530

    Full Backend Complete in MERN Stack

--- airbnb/app.js
+++ airbnb/app.js
@@ -6,6 +6,7 @@ const express = require('express');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const DB_PATH = "mongodb+srv://airbnb-clone:Gopal2008@airbnb.njdutq8.mongodb.net/?appName=airbnb";
+const { default: mongoose } = require('mongoose');
 
 //Local Module
 const storeRouter = require("./Routes/storeRouter")
@@ -13,7 +14,8 @@ const hostRouter = require("./Routes/hostRouter")
 const authRouter = require("./Routes/authRouter")
 const rootDir = require("./utils/pathUtil");
 const errorsController = require("./controllers/errors");
-const { default: mongoose } = require('mongoose');
+
+const multer = require("multer");
 
 const app = express();
 
@@ -25,7 +27,40 @@ const store = new MongoDBStore({
   collection: 'sessions'
 });
 
+const randomString = (length) =>{
+  const ch = 'qwertyuioplkjhgfdsazxcvbnm';
+  let result='';
+  for(let i=0;i<length;i++){
+    result+=ch.charAt(Math.floor(Math.random()*ch.length));
+  }
+  return result;
+}
+
+const storage = multer.diskStorage({
+  destination: (req,file,cb) =>{
+    cb(null, "uploads/");
+  },
+  filename: (req,file,cb) =>{
+    cb(null,randomString(10) + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req,file,cb)=>{
+  if(['image/jpeg','image/png','image/jpg'].includes(file.mimetype)){
+    cb(null,true);
+  }
+  else{
+    cb(null,false);
+  }
+};
+
 app.use(express.urlencoded());
+app.use(multer({storage,fileFilter}).single('photo'));
+app.use(express.static(path.join(rootDir, 'public')));
+app.use('/uploads',express.static(path.join(rootDir,"uploads")));
+app.use('/host/uploads',express.static(path.join(rootDir,"uploads")));
+app.use('/homes/uploads',express.static(path.join(rootDir,"uploads")));
+
 app.use(session({
   secret: "KnowledgeGate AI with Complete Coding",
   resave: false,
@@ -49,7 +84,6 @@ app.use("/host", (req, res, next) => {
 });
 app.use("/host", hostRouter);
 
-app.use(express.static(path.join(rootDir, 'public')))
 
 app.use(errorsController.pageNotFound);
 
